d5fa856a9932cec9b998b49f42ee79b38ff9e9f4
levom8 <levom8@gmail.com>
2022-01-29 17:12:08 -0500

    created the grid and added buttons

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # etch-a-sketch
+Browser version of something between a sketchpad and an Etch-A-Sketch.
+Creating a pretty neat toy for your portfolio to flex your DOM manipulation skills. 
---
+++ js/etch.js
@@ -0,0 +1,48 @@
+let number = 0;
+
+function numberSquare (number) {
+    for (let i = 0; i < number; i++) {
+        const i = document.createElement('div')
+        const container = document.querySelector('.grid');        
+        container.appendChild(i);
+        i.classList.add('square');
+
+            for (let j = 0; j < number; j++) {
+                const j = document.createElement('div');
+                i.appendChild(j);
+                j.classList.add('squareInside');
+          }
+
+        // squareHov is a node list. it looks and acts much like an array
+        const squareHov = document.querySelectorAll('.squareInside');
+        //we use the .forEach method to iterate through each square
+        squareHov.forEach((square) => {
+        //and for each one we add a 'mouseenter' listener
+            square.addEventListener("mouseover", (e) => { 
+                e.target.style.background = 'blue';
+                console.log(e)
+            });
+        }); // works with e or also just replace "e.target" by "square"
+    }
+}
+
+
+const btn = document.querySelector('.resetButton');
+btn.addEventListener("click", askNumber);
+function askNumber(e) {
+    number = prompt('Please enter a number',"type here");
+    if (number <= 100 & number > 0) {
+        return numberSquare(number);
+    }   
+    else {
+        alert("Please type a positive number under 100!")
+    }
+}
+
+const btnEfface = document.querySelector('.eraseButton');
+btnEfface.addEventListener("click", erase);
+function erase(e) {
+    return numberSquare(0) 
+    console.log(e)
+}
+
